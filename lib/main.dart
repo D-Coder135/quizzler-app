3ea38f53d78bcfe1a02b78688bcfff695254b86e
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-06 00:33:49 +0530

    Comment out some part of the code

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class _QuizPageState extends State<QuizPage> {
 
   Question question3 = Question('A slug\'s blood is green.', true);
 
-  List<bool> answersList = [false, true, true];
+  // List<bool> answersList = [false, true, true];
 
   @override
   Widget build(BuildContext context) {
