735e023cd8a57ef4b12a3a59b0787b4917a9113f
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-05 20:32:58 +0530

    add some fix up

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class _QuizPageState extends State<QuizPage> {
   List<String> questionsList = [
     'You can lead a cow down stairs but not up stairs.',
     'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
+    'A slug\'s blood is green.'
   ];
   List<Icon> scoreKeeper = [];
 
