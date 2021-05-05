210a93e15708d6a59088a13dd3512da4b550dd13
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-05 20:04:35 +0530

    add a setState() function inside the onPressed property of the FlatButton() widget

--- lib/main.dart
+++ lib/main.dart
@@ -90,6 +90,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                setState(() {});
               },
             ),
           ),
