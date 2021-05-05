de77fca22847b597320739c649a5a00efcb6c98c
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-05 20:31:32 +0530

    Add a question to the list questionsList

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,9 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<String> questionsList = [];
+  List<String> questionsList = [
+    'You can lead a cow down stairs but not up stairs.',
+  ];
   List<Icon> scoreKeeper = [];
 
   @override
@@ -112,7 +114,7 @@ class _QuizPageState extends State<QuizPage> {
 }
 
 /*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
+question1:  false,
 question2: 'Approximately one quarter of human bones are in the feet.', true,
 question3: 'A slug\'s blood is green.', true,
 */
