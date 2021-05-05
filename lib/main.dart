095d09eda1f09524115b08a61c1a37617cba7e03
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-06 00:44:34 +0530

    Change the name of the List

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class _QuizPageState extends State<QuizPage> {
   // Question question3 = Question('A slug\'s blood is green.', true);
 
   // List<bool> answersList = [false, true, true];
-  List<Question> questionsAnswersList = [
+  List<Question> questionBank = [
     Question('You can lead a cow down stairs but not up stairs.', false),
     Question('Approximately one quarter of human bones are in the feet.', true),
     Question('A slug\'s blood is green.', true)
